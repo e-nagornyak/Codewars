0e220c90635ab500d0d2b5d831ddfb82b4fe8ffe
Evhen Nagornyak <dolkiv@gmail.com>
2022-09-23 10:55:13 +0300

    completed the task

---
+++ Сodewars/Sentence_Smash-(8kyu)/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function smash(words) {
+   let result = [];
+   for (let i = 0; i < words.length; i++) {
+      result.push(words[i]);
+   }
+   console.log(result.join(" "));
+}
+smash(["hello", "amazing", "world"]);
