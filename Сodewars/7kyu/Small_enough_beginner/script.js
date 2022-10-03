cd702da61f5612ae106331658c7ab093bb4efec0
e-nagornyak <dolkiv@gmail.com>
2022-10-03 11:16:27 +0300

    Completed new task

--- Сodewars/7kyu/Small_enough_beginner/script.js
+++ Сodewars/7kyu/Small_enough_beginner/script.js
@@ -1,2 +1,16 @@
 "use strict";
 
+function openOrSenior(data) {
+   let a = [];
+   if (data.length === 0) {
+      return data;
+   }
+   for (let i = 0; i < data.length; i++) {
+      if (data[i][0] >= 55 && data[i][1] > 7) {
+         a.push('Senior');
+      } else {
+         a.push('Open');
+      }
+   }
+   return a;
+}
