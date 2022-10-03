7377782cfc5e42508537f0ec7227411c8fa4cd7c
e-nagornyak <dolkiv@gmail.com>
2022-10-03 21:35:52 +0300

    Added new task and completed

--- Сodewars/7kyu/Small_enough_beginner/script.js
+++ Сodewars/7kyu/Small_enough_beginner/script.js
@@ -1,16 +1,16 @@
 "use strict";
 
-function openOrSenior(data) {
-   let a = [];
-   if (data.length === 0) {
-      return data;
-   }
-   for (let i = 0; i < data.length; i++) {
-      if (data[i][0] >= 55 && data[i][1] > 7) {
-         a.push('Senior');
-      } else {
-         a.push('Open');
+function smallEnough(a, limit) {
+   let result = 0;
+   a.forEach((e) => {
+      if (e > limit) {
+         result += 1;
       }
-   }
-   return a;
+   })
+   return result > 0 ? false : true;
 }
+
+
+// function smallEnough(a, limit){
+//    return Math.max(...a) <= limit
+// }
