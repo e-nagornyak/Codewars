ecdb33da0a25bddcc1c23382662ddd90bab367c4
e-nagornyak <dolkiv@gmail.com>
2022-10-04 22:33:36 +0300

    Added another solution to the task

--- Сodewars/7kyu/Is_this_a_triangle/script.js
+++ Сodewars/7kyu/Is_this_a_triangle/script.js
@@ -8,3 +8,10 @@ function isTriangle(a, b, c) {
 }
 
 isTriangle(7,9,2)
+
+// function isTriangle(a,b,c)
+// {
+//     return a + b > c && a + c > b && c + b > a;
+// }
+//
+// var isTriangle = (a,b,c) => Math.max(a,b,c)<(a+b+c)/2
