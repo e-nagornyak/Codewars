7abe1fc0ceadeb2954186ac26721ed4b1963585a
e-nagornyak <dolkiv@gmail.com>
2022-10-09 00:00:54 +0300

    Added other solutions

--- Сodewars/7kyu/Round_up_to_the_next_multiple_of_5/script.js
+++ Сodewars/7kyu/Round_up_to_the_next_multiple_of_5/script.js
@@ -8,3 +8,12 @@ function roundToNext5(n) {
 }
 
 roundToNext5(-5)
+
+// function roundToNext5(n){
+//     return Math.ceil(n/5)*5;
+// }
+
+// function roundToNext5(n){
+//     while(n % 5 !== 0) n++;
+//     return n;
+// }
