71e8f6fe6e5edef826bfdaa2af939a35a3121266
e-nagornyak <dolkiv@gmail.com>
2022-10-09 17:11:43 +0300

    Added the solution using the method map

--- Сodewars/6kyu/Stop_gninnipS_My_sdroW/script.js
+++ Сodewars/6kyu/Stop_gninnipS_My_sdroW/script.js
@@ -10,3 +10,8 @@ function spinWords(string) {
     })
     return reverseArr.join(' ')
 }
+// function spinWords(words){
+//     return words.split(' ').map(function (word) {
+//         return (word.length > 4) ? word.split('').reverse().join('') : word;
+//     }).join(' ');
+// }
