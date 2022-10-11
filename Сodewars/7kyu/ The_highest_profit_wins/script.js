86fc3e5c10aa55988776741a62ffb72dae0e24eb
e-nagornyak <dolkiv@gmail.com>
2022-10-12 00:16:11 +0300

    Added other solution

--- Сodewars/7kyu/ The_highest_profit_wins/script.js
+++ Сodewars/7kyu/ The_highest_profit_wins/script.js
@@ -2,3 +2,7 @@ function minMax(arr) {
     let newArr = arr.sort((a, b) => a - b);
     return [newArr[0], newArr[newArr.length - 1]]
 }
+
+// function minMax(arr){
+//     return [Math.min(...arr), Math.max(...arr)];
+// }
