7c43d3b3b5dfd146f0ac2687f4f294c78a595fd1
e-nagornyak <dolkiv@gmail.com>
2022-10-19 00:24:35 +0300

    Updated the task and wrote an explanation for myself

--- Сodewars/7kyu/Remove_anchor_from_URL/script.js
+++ Сodewars/7kyu/Remove_anchor_from_URL/script.js
@@ -1,5 +1,9 @@
 function removeUrlAnchor(url) {
     let removeIndex = url.split('').findIndex((e) => e === '#');
     return (removeIndex < 1) ? url : url.slice(0, removeIndex)
-
 }
+
+
+// function removeUrlAnchor(url) {
+//     console.log(url.split('#')[0]);  вказуємо де саме розділювати елементи, тому ділимо посилання на 2 частини, і повертаємо першу
+// }
