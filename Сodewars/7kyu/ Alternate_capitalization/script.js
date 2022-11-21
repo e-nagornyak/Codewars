48d13ebf0b8d9477e1e6ed6a94c73dd48e38a734
e-nagornyak <dolkiv@gmail.com>
2022-11-21 23:53:07 +0200

    Added a solution using the method map

--- Сodewars/7kyu/ Alternate_capitalization/script.js
+++ Сodewars/7kyu/ Alternate_capitalization/script.js
@@ -9,3 +9,10 @@ function capitalize(s) {
 }
 
 capitalize('abcdef')
+
+
+// function capitalize(s){
+//     const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+//     const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+//     return [even, odd];
+// };
