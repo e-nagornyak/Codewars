a0cd30df42481a6402f2421ac50f19b75bc45836
e-nagornyak <dolkiv@gmail.com>
2022-12-01 20:23:17 +0200

    Added other solutions

--- Сodewars/7kyu/Beginner_Series_#3_Sum o__Numbers/script.js
+++ Сodewars/7kyu/Beginner_Series_#3_Sum o__Numbers/script.js
@@ -10,3 +10,14 @@ let getSum = (a, b) => {
 
 
 getSum(0, -1)
+
+const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+}
+
+function GetSum(a,b)
+{
+    return (Math.abs(a - b) + 1) * (a+b) / 2;
+}
