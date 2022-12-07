5495effccc55f221957a9b4993b27635eb297a03
e-nagornyak <dolkiv@gmail.com>
2022-12-08 01:21:17 +0200

    Completed new task

---
+++ Сodewars/7kyu/Remove_the_minimum/script.js
@@ -0,0 +1,11 @@
+function removeSmallest(numbers) {
+    let min = [...numbers].sort((a, b) => a - b)
+    let newArr = [...numbers]
+    delete newArr[newArr.indexOf(min[0])]
+    return newArr.filter(el => el !== undefined)
+}
+
+function removeSmallest(numbers) {
+    let indexOfMin = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
