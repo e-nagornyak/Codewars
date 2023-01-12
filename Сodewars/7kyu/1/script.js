e3e901f8acdb3d9461976223fdba51ed84d7f7bc
e-nagornyak <dolkiv@gmail.com>
2023-01-12 16:53:59 +0200

    Added new task

---
+++ Сodewars/7kyu/1/script.js
@@ -0,0 +1,9 @@
+function digitalRoot(n) {
+    let res
+    if (n < 10) {
+        return n
+    } else {
+        res = n.toString().split('').reduce((a, b) => +a + +b)
+    }
+    return digitalRoot(res)
+}
