83cb0dba6155b10f9e30af9cd6b67e817f3ce1c7
e-nagornyak <dolkiv@gmail.com>
2023-01-12 13:17:16 +0200

    Wrote other solution

--- Сodewars/7kyu/Jaden_Casing_Strings/script.js
+++ Сodewars/7kyu/Jaden_Casing_Strings/script.js
@@ -2,3 +2,10 @@ String.prototype.toJadenCase = function (string) {
     let arr = this.split(' ')
     return arr.map(w => w[0].toUpperCase() + w.slice(1)).join(' ')
 }
+
+// String.prototype.toJadenCase = function() {
+//     return this
+//         .split(" ")
+//         .map(i => i.replace(i[0], i[0].toUpperCase()))
+//         .join(" ");
+// };
