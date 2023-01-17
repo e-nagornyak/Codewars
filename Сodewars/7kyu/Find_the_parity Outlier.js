2d36a2df7fc5c8ee12e6825e01f0781845208738
e-nagornyak <dolkiv@gmail.com>
2023-01-17 22:58:07 +0200

    Refactoring task

--- Сodewars/7kyu/Find_the_parity Outlier.js
+++ Сodewars/7kyu/Find_the_parity Outlier.js
@@ -1,13 +1,8 @@
-function findOutlier(integers) {
-    let a = integers.filter(el => el % 2 !== 0)
-    let b = integers.filter(el => el % 2 === 0)
-    if (a.length === 1) {
-        return a[0]
-    } else {
-        return b[0]
-    }
+function findOutlier(int){
+    var even = int.filter(a=>a%2==0);
+    var odd = int.filter(a=>a%2!==0);
+    return even.length==1? even[0] : odd[0];
 }
-
 
 let a = [2, 2, 2, 1, 2, 4]
 findOutlier(a)
