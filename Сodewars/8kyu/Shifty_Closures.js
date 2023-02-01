2eb8fc9cecac7ef3060f59ead58447fbd6742ba8
e-nagornyak <dolkiv@gmail.com>
2023-02-01 16:39:10 +0200

    Completed a new tasks

---
+++ Сodewars/8kyu/Shifty_Closures.js
@@ -0,0 +1,10 @@
+var greet_abe = function () {
+    var name = 'Abe';
+    return "Hello, " + name + '!';
+};
+name = 'Ben';
+var greet_ben = function () {
+    return "Hello, " + name + '!';
+};
+console.log(greet_abe());
+console.log(greet_ben());
