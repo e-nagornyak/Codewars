57b2c49c4c14468da37ca820c869dab41ea9ee8b
e-nagornyak <dolkiv@gmail.com>
2023-03-08 20:46:17 +0200

    Add other solution

--- Сodewars/7kyu/Ones_adn_zeros.js
+++ Сodewars/7kyu/Ones_adn_zeros.js
@@ -1,7 +1,11 @@
 const binaryArrayToNumber = arr => {
-    let numStr = '';
-    arr.map(a=>String(a)).forEach(a=>{
-        numStr += a;
-    })
-    return parseInt(+(numStr),2);
+    // let numStr = '';
+    // arr.map(a=>String(a)).forEach(a=>{
+    //     numStr += a;
+    // })
+    // return parseInt(+(numStr),2);
+
+    return parseInt(arr.join(''), 2);
 };
+
+
