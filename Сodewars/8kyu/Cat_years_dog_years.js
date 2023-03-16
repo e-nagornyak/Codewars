c35394c029f9ee9cc93087f236f58ce2ebfe3649
e-nagornyak <dolkiv@gmail.com>
2023-03-16 22:23:46 +0200

    Added other solution

--- Сodewars/8kyu/Cat_years_dog_years.js
+++ Сodewars/8kyu/Cat_years_dog_years.js
@@ -15,3 +15,10 @@ var humanYearsCatYearsDogYears = function (humanYears) {
     return arr
 }
 console.log(humanYearsCatYearsDogYears(1));
+
+// other solution
+var humanYearsCatYearsDogYears = function(y) {
+    if (y == 1) return [1, 15, 15]
+    if (y == 2) return [2, 24, 24]
+    return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
+}
