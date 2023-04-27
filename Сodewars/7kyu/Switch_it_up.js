e89760935149e8958ba6b3961e78b0b99cfbda0b
Evhen Nagornyak <dolkiv@gmail.com>
2023-04-27 22:07:30 +0300

    Completed a new task

---
+++ Сodewars/7kyu/Switch_it_up.js
@@ -0,0 +1,6 @@
+function switchItUp(number) {
+    const arr = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+    return arr[number]
+}
+
+console.log(switchItUp(1));
