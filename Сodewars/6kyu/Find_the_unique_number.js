e17e5514a8d8c7cd2e1db226894bdb08edcdf539
Evhen Nagornyak <dolkiv@gmail.com>
2023-05-02 23:19:35 +0300

    Added other solution

--- Сodewars/6kyu/Find_the_unique_number.js
+++ Сodewars/6kyu/Find_the_unique_number.js
@@ -5,3 +5,20 @@ function findUniq(arr) {
     }
     return arr[arr.length - 1];
 }
+
+function findUniq(arr) {
+    const countMap = new Map();
+    for (let num of arr) {
+        if (countMap.has(num)) {
+            countMap.set(num, countMap.get(num) + 1);
+        } else {
+            countMap.set(num, 1);
+        }
+    }
+
+    for (let [num, count] of countMap) {
+        if (count === 1) {
+            return num;
+        }
+    }
+}
