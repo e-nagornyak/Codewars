a920a02ffa67f75a21a522fc9513f14eeaa81635
Evhen Nagornyak <dolkiv@gmail.com>
2023-05-04 23:55:56 +0300

    Refactoring

--- Сodewars/8kyu/Expressions_Matter.js
+++ Сodewars/8kyu/Expressions_Matter.js
@@ -1,12 +1,11 @@
 function expressionMatter(a, b, c) {
-    const values = [
+    return Math.max([
         a + b + c,
         a * b * c,
         a * (b + c),
         (a + b) * c,
         a + b * c,
         a * b + c
-    ];
-    return Math.max(...values);
+    ])
 }
 
