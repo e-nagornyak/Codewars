fd702c8054601d2ace55c4d5d1c453cb060cfabd
Evhen Nagornyak <dolkiv@gmail.com>
2023-06-17 23:20:01 +0300

    Refactoring

--- Сodewars/8kyu/Sum_of_Multlples.js
+++ Сodewars/8kyu/Sum_of_Multlples.js
@@ -1,12 +1,8 @@
 function sumMul(n, m) {
-    if (n <= 0 || m <= 0) {
-        return "INVALID";
-    }
-
+    if (n <= 0 || m <= 0) return "INVALID";
     let sum = 0;
     for (let i = n; i < m; i += n) {
         sum += i;
     }
-
     return sum;
 }
