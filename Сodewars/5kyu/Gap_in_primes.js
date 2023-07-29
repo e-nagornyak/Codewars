7042a2b6a634ec4f9196a4e1447e29928c5fdd85
Evhen Nagornyak <dolkiv@gmail.com>
2023-07-29 23:54:38 +0300

    Refactoring

--- Сodewars/5kyu/Gap_in_primes.js
+++ Сodewars/5kyu/Gap_in_primes.js
@@ -2,19 +2,15 @@ function gap(g, m, n) {
     let prevPrime = null;
 
     const isPrime = (num) =>  {
-        if (num < 2) return false;
-        for (let i = 2; i <= Math.sqrt(num); i++) {
+        for (let i = 2; i * i <= num; i++) {
             if (num % i === 0) return false;
         }
         return true;
     }
 
-
     for (let num = m; num <= n; num++) {
         if (isPrime(num)) {
-            if (prevPrime !== null && num - prevPrime === g) {
-                return [prevPrime, num];
-            }
+            if (prevPrime !== null && num - prevPrime === g) return [prevPrime, num];
             prevPrime = num;
         }
     }
