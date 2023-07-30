1d8d5408c581e51f235e0d5a08fba7e091f067b4
Evhen Nagornyak <dolkiv@gmail.com>
2023-07-30 20:50:11 +0300

    Added other solution

--- Сodewars/6kyu/Build_tower.js
+++ Сodewars/6kyu/Build_tower.js
@@ -10,3 +10,10 @@ function towerBuilder(lines) {
     }
     return tower;
 }
+
+function towerBuilder(n) {
+    return Array.from({length: n}, function(v, k) {
+        const spaces = ' '.repeat(n - k - 1);
+        return spaces + '*'.repeat(k + k + 1) + spaces;
+    });
+}
