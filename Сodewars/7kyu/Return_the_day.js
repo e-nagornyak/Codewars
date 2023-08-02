a33f6162671d71be57d3680fd0d021e3f13ee412
Evhen Nagornyak <dolkiv@gmail.com>
2023-08-02 22:35:10 +0300

    Completed the task

--- Сodewars/7kyu/Return_the_day.js
+++ Сodewars/7kyu/Return_the_day.js
@@ -1,4 +1,4 @@
-function whatday(num) {
+function WhatDay(num) {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     return days[--num] || 'Wrong, please enter a number between 1 and 7'
 }
