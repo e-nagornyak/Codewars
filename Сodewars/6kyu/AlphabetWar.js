e8706a52a48ddb6a0d14b35ee6bab966ab299741
Evhen Nagornyak <dolkiv@gmail.com>
2023-08-06 22:54:47 +0300

    Refactoring

--- Сodewars/6kyu/AlphabetWar.js
+++ Сodewars/6kyu/AlphabetWar.js
@@ -1,17 +1,5 @@
 function alphabetWar(fight) {
-    const obj = {w: 4, p: 3, b: 2, s: 1, m: 4, q: 3, d: 2, z: 1}
-    const finishText = ['Right side wins!', "Left side wins!", "Let's fight again!"]
-
-    const sum = (req) => fight.replace(req, '').split('').reduce((a, b) => a + obj[b], 0)
-    const left = sum(/[^wpbs]/g)
-    const right = sum(/[^mqdz]/g)
-
-    switch (true) {
-        case left > right:
-            return finishText[1]
-        case right > left:
-            return finishText[0]
-        default:
-            return finishText[2]
-    }
+    const obj = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+    const res = fight.split('').reduce((a, b) => a + (obj[b] || 0), 0);
+    return res ? (res < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
 }
