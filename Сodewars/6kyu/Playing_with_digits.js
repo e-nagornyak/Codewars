8b5075061d9a896c2fc9281c8a67bcb8534f20e1
Evhen Nagornyak <dolkiv@gmail.com>
2023-08-06 23:01:50 +0300

    More refactoring

--- Сodewars/6kyu/Playing_with_digits.js
+++ Сodewars/6kyu/Playing_with_digits.js
@@ -1,5 +1,4 @@
 function digPow(n, p) {
-    const getDigits = (num) => num.toString().split('').map(Number);
-    const totalSum = getDigits(n).reduce((sum, digit, i) => sum + Math.pow(digit, p + i), 0);
+    const totalSum = String(n).split("").reduce((s, d, i) => s + Math.pow(d, p + i), 0)
     return totalSum % n ? -1 : totalSum / n
 }
