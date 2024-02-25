81f551e75452a8b836c6621039bc41276c573946
e.nagornyak <dolkiv@gmail.com>
2024-02-25 21:56:29 +0200

    () - fix jest

--- src/js/test.test.js
+++
@@ -1,5 +0,0 @@
-import {titleCase} from './test'
-
-test('main test', () => {
-    expect(titleCase('First a of in', 'an often into')).toBe("bla");
-});
--- src/ts/test.test.ts
+++ src/ts/test.test.ts
@@ -1,8 +1,28 @@
-import {describe, expect, test} from '@jest/globals';
-import {titleCase} from './test';
+import { describe, expect, test } from '@jest/globals';
 
-describe('sum module', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(titleCase("bla", "bla")).toBe(3);
-    });
+function lengthOfLongestSubstring(s: string): number {
+  let length = 0;
+  const charMap = {};
+
+  for (let start = 0, end = 0; end < s.length; end++) {
+    const currentChar = s[end];
+    if (charMap[currentChar] !== undefined && charMap[currentChar] >= start) {
+      start = charMap[currentChar] + 1;
+    }
+    charMap[currentChar] = end;
+    length = Math.max(length, end - start + 1);
+  }
+
+  return length;
+}
+
+
+describe("Solution", () => {
+  test("lengthOfLongestSubstring", () => {
+    expect(lengthOfLongestSubstring("abcabcbb")).toBe(3);
+    expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
+    expect(lengthOfLongestSubstring("pwwkew")).toBe(3);
+    // expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
+  });
 });
+
