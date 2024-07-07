4612f210f3032fcef58fdb53d6f3f24ccfdb5293
e.nagornyak <dolkiv@gmail.com>
2024-07-07 22:12:12 +0300

    () - solved the new test

--- src/ts/test.test.ts
+++ src/ts/test.test.ts
@@ -1,13 +1,13 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test } from "@jest/globals";
 
-function lengthOfLastWord(s: string): number {
-  return s?.trim()?.split(' ')?.at(-1)?.length
-}
+function defangIPaddr(address: string): string {
+  return address?.replace(/\./g, '[.]')
+};
 
+// Test cases
 describe("Solution", () => {
   test("lengthOfLongestSubstring", () => {
-    expect(lengthOfLastWord("   fly me   to   the moon  ")).toBe(4);
-    // expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
+    expect(defangIPaddr("1.1.1.1")).toBe("1[.]1[.]1[.]1");
+    expect(defangIPaddr("255.100.50.0")).toBe("255[.]100[.]50[.]0");
   });
 });
-
