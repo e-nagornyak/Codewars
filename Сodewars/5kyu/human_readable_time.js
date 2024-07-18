cebb6123f1d4dec01037daa5207436e6b9a9025f
e.nagornyak <dolkiv@gmail.com>
2024-07-18 22:55:21 +0300

    () - solved the new test

---
+++ Сodewars/5kyu/human_readable_time.js
@@ -0,0 +1,19 @@
+/**
+ * @description
+ * Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+ *
+ * HH = hours, padded to 2 digits, range: 00 - 99
+ * MM = minutes, padded to 2 digits, range: 00 - 59
+ * SS = seconds, padded to 2 digits, range: 00 - 59
+ * The maximum time never exceeds 359999 (99:59:59)
+ *
+ * You can find some examples in the test fixtures.
+ */
+
+function humanReadable(seconds) {
+  const hours = String(Math.floor(seconds / 3600)).padStart(2, "0");
+  const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+  const secs = String(seconds % 60).padStart(2, '0');
+
+  return `${hours}:${minutes}:${secs}`;
+}
